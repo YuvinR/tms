5df0e05c3dfc381818d613a02ddffddadc920e04
tdwic <36078972+tdwic@users.noreply.github.com>
2020-08-19 22:36:13 +0530

    new interface added

---
+++ TimeTableManagementSystem/interfaces/ManageWorkingDaysAndHours/ManageWorkignDaysAndHours.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimeTableManagementSystem.interfaces.ManageWorkingDaysAndHours
+{
+    /// <summary>
+    /// Interaction logic for ManageWorkignDaysAndHours.xaml
+    /// </summary>
+    public partial class ManageWorkignDaysAndHours : Window
+    {
+        public ManageWorkignDaysAndHours()
+        {
+            InitializeComponent();
+        }
+    }
+}
