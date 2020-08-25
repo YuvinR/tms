eb912d5c518e7becc1327f925f6f20b9cb32162c
Dewsara <dewsara6@gmail.com>
2020-08-25 09:40:11 +0530

    DB Table Commands

--- TimeTableManagementSystem/StartupConfigurations.cs
+++ TimeTableManagementSystem/StartupConfigurations.cs
@@ -69,6 +69,72 @@ namespace TimeTableManagementSystem
                     connection.Close();
                 }
 
+                if (!checkIfExist("Lecturer"))
+                {
+                    connection.Open();
+                    SQLiteCommand cmd = connection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    /*cmd.CommandText = "CREATE TABLE Lecturer ( " +
+                                        "EmployeeID    TEXT, " +
+                                        "Name  TEXT, " +
+                                        "Faculty   TEXT, " +
+                                        "Department    TEXT, " +
+                                        "Center    TEXT, " +
+                                        "Building  TEXT, " +
+                                        "Level TEXT, " +
+                                        "Rank  REAL, " +
+                                        "PRIMARY KEY(EmployeeID));";*/
+
+                    cmd.CommandText = "CREATE TABLE Lecturer (EmployeeID TEXT Primay Key, Name  TEXT, Faculty TEXT, Department TEXT, Center TEXT, Building  TEXT, Level TEXT, Rank REAL);";
+                                      
+
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+                if (!checkIfExist("Subject"))
+                {
+                    connection.Open();
+                    SQLiteCommand cmd = connection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    /*cmd.CommandText = "CREATE TABLE Subject ( " +
+                                        "Subject_Code  TEXT, " +
+                                        "Subject_Name TEXT, " +
+                                        "Offered_Year  TEXT, " +
+                                        "Offered_Semester   TEXT, " +
+                                        "Lecture_Hours  INTEGER, " +
+                                        "Tutorial_Hours INTEGER, " +
+                                        "Lab_Hours INTEGER, " +
+                                        "Evaluation_Hours  INTEGER, " +
+                                        "PRIMARY KEY(Subject_Code));";*/
+
+                    cmd.CommandText = "CREATE TABLE Subject (Subject_Code TEXT Primary Key, Subject_Name TEXT, Offered_Year TEXT, Offered_Semester TEXT, Lecture_Hours INTEGER, Tutorial_Hours INTEGER, Lab_Hours INTEGER, Evaluation_Hours  INTEGER);";
+
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                if (!checkIfExist("Sessions"))
+                {
+                    connection.Open();
+                    SQLiteCommand cmd = connection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    /*cmd.CommandText = "CREATE TABLE 'Sessions' ( " +
+                                        "Session_ID  INTEGER Autoincrement, " +
+                                        "Lecturers TEXT, " +
+                                        "Subject  TEXT, " +
+                                        "Subject_Code   TEXT, " +
+                                        "Tag  TEXT, " +
+                                        "GroupID TEXT, " +
+                                        "Student_Count INTEGER, " +
+                                        "Duration  INTEGER, " +
+                                        "PRIMARY KEY(Session_ID));";*/
+
+                    cmd.CommandText = "CREATE TABLE Sessions(Session_ID INTEGER Primary Key Autoincrement, Lecturers TEXT, Subject TEXT, Subject_Code TEXT, Tag TEXT, GroupID TEXT, Student_Count INTEGER,Duration  INTEGER);";
+
+
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
             }
             catch(Exception ex)
             {
