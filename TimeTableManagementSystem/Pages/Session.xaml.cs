544eb8ee1d4b59458a5fb3c873bdabc59cade73c
Dewsara <dewsara6@gmail.com>
2020-09-03 15:28:35 +0530

    Add Session button added to Dashboard

--- TimeTableManagementSystem/Pages/Session.xaml.cs
+++ TimeTableManagementSystem/Pages/Session.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimeTableManagementSystem.interfaces.Sessions;
 
 namespace TimeTableManagementSystem.Pages
 {
@@ -21,6 +22,12 @@ namespace TimeTableManagementSystem.Pages
         public Session()
         {
             InitializeComponent();
+        }
+
+        private void BtnAddSession(object sender, RoutedEventArgs e)
+        {
+            Add_Sessions add_Sessions = new Add_Sessions();
+            add_Sessions.Show();
         }
     }
 }
