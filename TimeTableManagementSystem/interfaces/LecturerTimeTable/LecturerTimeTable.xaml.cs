b3fdf09e97bd52c7369d97dc1ee9b79f9052fa9a
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-09-14 20:17:43 +0530

    lecturer loading part added

--- TimeTableManagementSystem/interfaces/LecturerTimeTable/LecturerTimeTable.xaml.cs
+++ TimeTableManagementSystem/interfaces/LecturerTimeTable/LecturerTimeTable.xaml.cs
@@ -9,6 +9,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SQLite;
+using TimeTableManagementSystem.DB_Config;
 
 namespace TimeTableManagementSystem.interfaces.LecturerTimeTable
 {
@@ -17,9 +20,60 @@ namespace TimeTableManagementSystem.interfaces.LecturerTimeTable
     /// </summary>
     public partial class LecturerTimeTable : Window
     {
+        private SQLiteConnection connection = db_config.connect();
         public LecturerTimeTable()
         {
             InitializeComponent();
+            fetchDataToComboBox();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_generate_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(cmbLecturerName.SelectedValue.ToString());
+        }
+
+        public void fetchDataToComboBox()
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+
+                SQLiteCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+
+                command.CommandText = "Select * from Lecturer";
+
+                SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command);
+
+                DataSet ds = new DataSet();
+
+                dataAdapter.Fill(ds, "Lecturer");
+
+                //foreach(DataRow dr in dataTable.Rows)
+                //{
+
+                //    comboacys.Items.Add(dr["Year"].ToString());
+                //}
+                cmbLecturerName.ItemsSource = ds.Tables[0].DefaultView;
+                cmbLecturerName.DisplayMemberPath = ds.Tables[0].Columns["Name"].ToString();
+                cmbLecturerName.SelectedValuePath = ds.Tables[0].Columns["EmployeeID"].ToString();
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
     }
 }
